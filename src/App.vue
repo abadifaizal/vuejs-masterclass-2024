b790c8737f704442b6b7c63968d0c07075a2187b
abadifaizal <abadifaizal@gmail.com>
2024-05-29 20:14:34 +0900

    adding vue router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Welcome to pulse!</h1>
+  <RouterView />
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/views/HomeView.vue')
+    },
+    {
+      path: '/projects',
+      name: 'Projects',
+      component: () => import('@/views/ProjectsView.vue')
+    },
+    {
+      path: '/projects/:id',
+      name: 'Project',
+      component: () => import('@/views/SingleProject.vue')
+    }
+  ]
 })
 
 export default router
