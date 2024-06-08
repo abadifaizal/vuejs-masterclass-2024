b280816220f8203b9f6bd98b3e5c03b19fa63b0d
abadifaizal <abadifaizal@gmail.com>
2024-06-08 13:49:27 +0900

    fix routing and add supabase config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import('@/views/HomeView.vue')
-    },
-    {
-      path: '/projects',
-      name: 'Projects',
-      component: () => import('@/views/ProjectsView.vue')
-    },
-    {
-      path: '/projects/:id',
-      name: 'Project',
-      component: () => import('@/views/SingleProject.vue')
-    }
-  ]
+  routes
 })
 
 export default router
